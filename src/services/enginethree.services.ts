2368e9c0a394c4c4813c0b79b980c9a338c201a4
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-07-14 10:12:42 -0500

    feat: añado servicio encargado de parar el motor 3

--- src/services/enginethree.services.ts
+++ src/services/enginethree.services.ts
@@ -3,5 +3,8 @@ const ESP32_BASE_URL = "http://192.168.4.1";
 export const engineThreeServices = {
     startEngine: async () => {
         await fetch(`${ESP32_BASE_URL}/start3`)
+    },
+    stopEngine: async () => {
+        await fetch(`${ESP32_BASE_URL}/stop3`)
     }
 }
