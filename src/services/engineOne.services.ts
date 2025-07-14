3f30737dc745e662097ff19aa0bc4299f8f7b073
Julian Rivera <juli.rivera.quintero@gmail.com>
2025-07-14 09:59:02 -0500

    feat: añado servicio encargado de parar el motor

--- src/services/engineOne.services.ts
+++ src/services/engineOne.services.ts
@@ -3,7 +3,10 @@ const ESP32_BASE_URL = "http://192.168.4.1";
 
 
 export const engineOneServices = {
-    startEngine: async (id: number) => {
+    startEngine: async () => {
         await fetch(`${ESP32_BASE_URL}/start1`)
+    },
+    stopEngine: async () => {
+        await fetch(`${ESP32_BASE_URL}/stop1`)
     }
 }
